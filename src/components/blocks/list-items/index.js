8216548c2308e8b536154d97deecdf6b0c468f72
shivangiupadhyay29 <reach.upadhyay.shivangi@gmail.com>
2020-09-06 22:34:17 +0530

    updating title

--- src/components/blocks/list-items/index.js
+++ src/components/blocks/list-items/index.js
@@ -11,9 +11,15 @@ export default function Listing({
   if (item && Array.isArray(label)) {
     return (
       <li key={item[keyProp]} className="list-bullets" tabindex={item.id + 2}>
-        <span className="label-size">{item[label[0]]}</span>
-        <span className="label-size">{item[label[1]]}</span>
-        <span className="label-size ellipsis">{item[label[2]]}</span>
+        <span className="label-size" title={item[label[0]]}>
+          {item[label[0]]}
+        </span>
+        <span className="label-size" title={item[label[1]]}>
+          {item[label[1]]}
+        </span>
+        <span className="label-size ellipsis" title={item[label[2]]}>
+          {item[label[2]]}
+        </span>
       </li>
     );
   }
