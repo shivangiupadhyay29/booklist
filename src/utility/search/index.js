30bb40957ebed3f7a89187f2507c8713ec1d327a
shivangiupadhyay29 <reach.upadhyay.shivangi@gmail.com>
2020-09-12 22:17:49 +0530

    search utility function improvmenet.

--- src/utility/search/index.js
+++ src/utility/search/index.js
@@ -6,28 +6,11 @@ const schema = (key) => {
   return dataByProperty(key);
 };
 
-// search function
-// function searchBooksByCharacters(search) {
-//   // gathers the titles and authors from the data JSON.
-//   const titles = schema(constant.TITLES);
-//   const authors = schema(constant.AUTHORS);
-
-//   //here the map adds the title property into the final summaries array
-//   return (
-//     schema(constant.SUMMARIES)
-//       ?.map((item, index) => ({
-//         ...item,
-//         title: titles[index],
-//         author: authors[index][constant.AUTHOR]
-//       }))
-//       //filter is to filter out the item with matches string
-//       ?.filter((item) => item.summary.includes(search))
-//   );
-// }
-
+//check that the items are pre-existing or not in resultant array...
 const isMappedAlready = (array, item) =>
   array.indexOf(item) === -1 ? false : true;
 
+//pre-processing the summaries of all the book collection
 function preProcessigStrings(summaries, titles, authors) {
   let hashMap = {};
   let summaryArray = summaries?.map((item) => item.summary);
@@ -53,11 +36,13 @@ function preProcessigStrings(summaries, titles, authors) {
   return hashMap;
 }
 
+//split the sentences of search content lookup into words
 const keywordsFromText = (search) => {
   const words = search.split(' ');
   return words;
 };
 
+//searching main functions
 function searchBooksByCharacters(search) {
   let result;
   const keywords = keywordsFromText(search);
